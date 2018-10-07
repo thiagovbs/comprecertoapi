7ad24331aa6c016c3e4257a58d3d4c266fb8507d
Elias Braz <elias.brazjunior@gmail.com>
2018-10-07 17:23:55 -0300

    - Implementação do Spring Security com OAuth2

---
+++ src/main/java/br/com/comprecerto/api/security/AppUserDetailsService.java
@@ -0,0 +1,41 @@
+package br.com.comprecerto.api.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.comprecerto.api.entities.Usuario;
+import br.com.comprecerto.api.repositories.UsuarioRepository;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Optional<Usuario> usuarioOptional = repository.findByLogin(login);
+		Usuario usuario = usuarioOptional
+				.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha inválidos!"));
+		return new User(login, usuario.getSenha(), getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		usuario.getPermissoes()
+				.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
+		return authorities;
+	}
+
+}
--- src/main/java/br/com/comprecerto/api/security/AuthorizationServerConfig.java
+++ src/main/java/br/com/comprecerto/api/security/AuthorizationServerConfig.java
@@ -1,40 +1,47 @@
 package br.com.comprecerto.api.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
-	private TokenStore tokenStore;
-
-	@Autowired
-	private UserApprovalHandler userApprovalHandler;
-
-	@Autowired
-	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("angular").authorizedGrantTypes("password").scopes("read", "write")
-				.secret("{noop}@ngul@r").accessTokenValiditySeconds(1800).refreshTokenValiditySeconds(3 * 1800);
+		clients.inMemory().withClient("angular").secret("@ngul@r0").scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
+				.refreshTokenValiditySeconds(3600 * 24);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
+		endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter()).reuseRefreshTokens(false)
 				.authenticationManager(authenticationManager);
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("comprecerto");
+		return accessTokenConverter;
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 }
