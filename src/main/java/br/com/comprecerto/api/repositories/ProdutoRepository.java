ab805a74366bf89f1b2aa4519de93ee72567ecf5
Elias Braz <elias.brazjunior@gmail.com>
2019-01-27 12:30:30 -0200

    Consulta de marcas por subcategoria

--- src/main/java/br/com/comprecerto/api/repositories/ProdutoRepository.java
+++ src/main/java/br/com/comprecerto/api/repositories/ProdutoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.comprecerto.api.entities.Produto;
+import br.com.comprecerto.api.entities.Subcategoria;
 import br.com.comprecerto.api.repositories.produto.ProdutoRepositoryQuery;
 
 @Repository
@@ -17,5 +18,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer>, Prod
 
 	@Query("from Produto p where p.subcategoria.categoria.idCategoria = ?1")
 	List<Produto> buscarProdutosPorCategoria(Integer idCategoria);
+
+	@Query("select distinct p.marca from Produto p where p.subcategoria = ?1")
+	List<String> buscarMarcasPorSubcategoria(Subcategoria subcategoria);
 
 }
