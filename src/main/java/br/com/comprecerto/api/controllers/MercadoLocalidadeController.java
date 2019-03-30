0023c93445d360856a86827c4f1dd721f7c8c4e5
Elias Braz <elias.brazjunior@gmail.com>
2019-03-30 13:10:01 -0300

    Inclusão de CORS no controller

--- src/main/java/br/com/comprecerto/api/controllers/MercadoLocalidadeController.java
+++ src/main/java/br/com/comprecerto/api/controllers/MercadoLocalidadeController.java
@@ -4,12 +4,14 @@ import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.comprecerto.api.services.MercadoLocalidadeService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/mercado-localidades")
 public class MercadoLocalidadeController {
