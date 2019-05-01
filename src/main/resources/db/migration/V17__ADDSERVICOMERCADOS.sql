5d84d447999c9220010b82209c7a20923c07cbbe
Thiago Veloso <thiago.vbs@gmail.com>
2019-05-01 12:08:52 -0300

    correçao do arquivo de insert na base de dados

--- src/main/resources/db/migration/V17__ADDSERVICOMERCADOS.sql
+++ src/main/resources/db/migration/V17__ADDSERVICOMERCADOS.sql
@@ -1,8 +1,8 @@
 INSERT INTO sheap.servico (nome, f_ativo, dt_criacao, dt_alteracao) VALUES ('Sheap Direct', true, current_date, current_date);
 
-SELECT pg_catalog.setval('sheap.servico_id_servico_seq', 101, false);
+SELECT pg_catalog.setval('sheap.servico_id_servico_seq', 15, false);
 
 INSERT INTO sheap.pacote_servico (f_ativo, nome, descricao, valor, id_servico, dt_criacao, dt_alteracao) VALUES (true, 'Entrega em Casa', 'x', 0, (SELECT id_servico FROM sheap.servico WHERE nome = 'Sheap Direct'), current_date, current_date);
 INSERT INTO sheap.pacote_servico (f_ativo, nome, descricao, valor, id_servico, dt_criacao, dt_alteracao) VALUES (true, 'Retirada na Loja', 'x', 0, (SELECT id_servico FROM sheap.servico WHERE nome = 'Sheap Direct'), current_date, current_date);
 
-SELECT pg_catalog.setval('sheap.pacote_servico_id_pacote_servico_seq', 102, false);
+SELECT pg_catalog.setval('sheap.pacote_servico_id_pacote_servico_seq', 27, false);
