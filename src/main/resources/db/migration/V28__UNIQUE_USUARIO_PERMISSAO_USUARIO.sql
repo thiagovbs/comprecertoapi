a1b51cda1ad6925bbed4937619cada6e8fe84e3f
Vinicius <vinicius.valenca@outlook.com.br>
2019-06-05 16:54:11 -0300

    Fix Bug Cadastro Nome + Permissao Usuario V2

--- src/main/resources/db/migration/V28__UNIQUE_USUARIO_PERMISSAO_USUARIO.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ONLY sheap.usuario DROP CONSTRAINT usuario_nome_key;
-INSERT INTO sheap.permissao (descricao) VALUES ('USUARIO');
-
-
---
+++ src/main/resources/db/migration/V29__UNIQUE_USUARIO_PERMISSAO_USUARIO.sql
@@ -0,0 +1,3 @@
+INSERT INTO sheap.permissao (descricao) VALUES ('USUARIO');
+
+
