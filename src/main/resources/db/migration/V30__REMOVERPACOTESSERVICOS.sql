52d150c1f6f7314ff893334bdef61a1487e43a54
Vinicius <vinicius.valenca@outlook.com.br>
2019-06-26 23:08:37 -0300

    Scripts Alterar/Remover Pacotes

---
+++ src/main/resources/db/migration/V30__REMOVERPACOTESSERVICOS.sql
@@ -0,0 +1,7 @@
+DELETE FROM sheap.pacote_servico WHERE id_servico=9;
+DELETE FROM sheap.servico WHERE id_servico=9;
+
+DELETE FROM sheap.pacote_servico WHERE id_servico=100;
+DELETE FROM sheap.servico WHERE id_servico=100;
+
+
